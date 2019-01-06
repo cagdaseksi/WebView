2f54e1368626ba9438bb2a7d19c163ca3c76edbb
cagdaseksi <cagdas.eksi@hotmail.com>
2019-01-06 17:11:50 +0300

    initial commit.

--- WebLoad/ViewController.swift
+++ WebLoad/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = URL(string: "http://www.nytimes.com")
+        webView.loadRequest(URLRequest(url: url!))
+        
     }
 
 
